dc9e0e5d97b7a04eb8ec9df1f786a5d899753c96
Joey A <jaxtell@amfam.com>
2022-08-31 18:34:51 +0000

    Added the first label

--- main.tf
+++ main.tf
@@ -64,10 +64,11 @@ resource "google_compute_instance" "hashicat" {
     ssh-keys = "ubuntu:${chomp(tls_private_key.ssh-key.public_key_openssh)} terraform"
   }
 
-  tags = ["http-server", "department":"devops"]
+  tags = ["http-server"]
 
   labels = {
     name = "hashicat"
+    department = "devops"
   }
 
 }
